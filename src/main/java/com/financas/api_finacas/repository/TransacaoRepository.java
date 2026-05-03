e6551a7207370b8570e4ca67df36e8ff60a040ce
Artur Chagas <arturchagax@gmail.com>
2026-05-03 02:10:14 -0300

    adicionando o pacote repository e criando a interface TransacaoRepository

---
+++ src/main/java/com/financas/api_finacas/repository/TransacaoRepository.java
@@ -0,0 +1,27 @@
+package com.financas.api_finacas.repository;
+
+import com.financas.api_finacas.model.TipoTransacao;
+import com.financas.api_finacas.model.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    List<Transacao> findByTipo(TipoTransacao tipo);
+
+    List<Transacao> findByCategoria(String categoria);
+
+    List<Transacao> findByDataCriacaoBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    @Query("SELECT SUM(t.valor) FROM Transacao t WHERE t.tipo =:tipo")
+    BigDecimal  sumByTipo(@Param("tipo") TipoTransacao tipo);
+
+
+}

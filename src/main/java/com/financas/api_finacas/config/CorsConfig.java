5a1e490b4fc2cb7238314810cbe4093b6bc42cf0
Artur Chagas <arturchagax@gmail.com>
2026-05-06 11:40:30 -0300

    configurando cors

--- src/main/java/com/financas/api_finacas/config/CorsConfig.java
+++ src/main/java/com/financas/api_finacas/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.financas.api_finacas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,12 +10,16 @@ public class CorsConfig  implements WebMvcConfigurer {
 
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/api/**")
-               .allowedOrigins("*")
-               .allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS")
-               .allowedHeaders("*")
-               .allowCredentials(false);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }

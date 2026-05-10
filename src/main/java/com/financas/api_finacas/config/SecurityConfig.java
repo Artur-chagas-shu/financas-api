14d3fce6d07f50af97bd22996faa653252552489
Artur Chagas <126296693+Artur-chagas-shu@users.noreply.github.com>
2026-05-10 00:54:12 -0300

    Update SecurityConfig.java

--- src/main/java/com/financas/api_finacas/config/SecurityConfig.java
+++ src/main/java/com/financas/api_finacas/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**","/api/health").permitAll()
+                        .requestMatchers("/api/auth/**","/api/health**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(

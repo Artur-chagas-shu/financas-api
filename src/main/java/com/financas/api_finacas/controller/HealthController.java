72560693101c77ce9baf040e19a7a08b919c5a7b
Artur Chagas <126296693+Artur-chagas-shu@users.noreply.github.com>
2026-05-10 02:19:58 -0300

    Update HealthController.java

--- src/main/java/com/financas/api_finacas/controller/HealthController.java
+++ src/main/java/com/financas/api_finacas/controller/HealthController.java
@@ -2,6 +2,7 @@ package com.financas.api_finacas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
